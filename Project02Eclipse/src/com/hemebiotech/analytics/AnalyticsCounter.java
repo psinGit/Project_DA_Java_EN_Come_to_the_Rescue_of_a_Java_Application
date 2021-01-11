d7ce1bb32d190f14c4f5547ef308a28664eec82d
psin <psin@oc.edu>
2021-01-11 20:09:03 +0100

    BugFix

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,33 +5,34 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
+	private static int pupilCount = 0;		
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
 		while (line != null) {
-			i++;	// increment i
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
+				System.out.println("number of rashCount: " + rashCount);
+
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
+				System.out.println("number of pupilCount: " + pupilCount);
+
 			}
 
 			line = reader.readLine();	// get another symptom
 		}
+		reader.close();
 		
 		// next generate output
 		FileWriter writer = new FileWriter ("result.out");
